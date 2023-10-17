162c00f7688046d9f3c5c2a77d163bb73acaf8dc
Ankit Singhal <thejiteshsinghal@gmail.com>
2023-10-17 21:05:47 +0530

    ok

---
+++ Day22/Ankit_Singhal/Transitive Closure of a Graph.java
@@ -0,0 +1,31 @@
+class Solution{
+    static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][]) {
+        int[][] adj = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] == 1 && i != j) {
+                    adj[i][j] = 1;
+                }
+            }
+        }
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ans.add(new ArrayList<>(n));
+            for (int j = 0; j < n; j++) {
+                ans.get(i).add(0);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(i, i, adj, ans);
+        }
+        return ans;
+    }
+    static void dfs(int i, int j, int[][] adj,  ArrayList<ArrayList<Integer>> ans) {
+        ans.get(i).set(j, 1);
+        for (int it = 0; it < adj[j].length; it++) {
+            if (adj[j][it] == 1 && ans.get(i).get(it) == 0) {
+                dfs(i, it, adj, ans);
+            }
+        }
+    }
+}
---
+++ Day22/problem_statement.md
@@ -0,0 +1,6 @@
+## Day22
+
+GeeksforGeeks: Transitive Closure of a Graph
+[Solve Now](https://practice.geeksforgeeks.org/problems/transitive-closure-of-a-graph0930/1)
+
+LeetCode: Validate Binary Tree Nodes [Solve Now](https://leetcode.com/problems/validate-binary-tree-nodes/)
