232840c718b2d04043d94d0f86ca8c42892f267a
Ankit Singhal <thejiteshsinghal@gmail.com>
2023-11-11 19:02:29 +0530

    ok

---
+++ Day47/Ankit Singhal/Isomorphic Strings.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    //Function to check if two strings are isomorphic.
+    public static boolean areIsomorphic(String str1,String str2)
+    {
+        if(str1.length() != str2.length())
+            return false;
+        
+        HashMap<Character, Character> m = new HashMap<>();
+        
+        for(int i=0;i<str1.length();i++){
+            if(m.containsKey(str1.charAt(i))){
+                if(m.get(str1.charAt(i)) != str2.charAt(i)){
+                    return false;
+                }
+            }else{
+                if(m.containsValue(str2.charAt(i))){
+                    return false;
+                }
+                m.put(str1.charAt(i), str2.charAt(i));
+            }
+        }
+        return true;
+    }
+}
--- Day47/Problem_Statement.md
+++ Day47/Problem_Statement.md
@@ -2,3 +2,7 @@
 
 Leetcode: Design Graph With Shortest Path Calculator
 [Solve Now](https://leetcode.com/problems/design-graph-with-shortest-path-calculator/description/?envType=daily-question&envId=2023-11-11)
+
+
+GeeksForGeeks: Isomorphic Strings 
+[Solve Now](https://www.geeksforgeeks.org/problems/isomorphic-strings-1587115620/1)
