1c441553b616b3416373621fb5bdc50fb482bfd4
Ankit Singhal <thejiteshsinghal@gmail.com>
2023-11-23 17:43:01 +0530

    ok

---
+++ Day59/Ankit Singhal/Arithmetic Subarrays.java
@@ -0,0 +1,26 @@
+class Solution {
+        public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> res = new ArrayList<>();
+        for (int i=0; i<l.length; i++) {
+            int start = l[i];
+            int end = r[i];
+            List<Integer> temp = new ArrayList<>();
+            for (int j=start; j<end+1; j++) {
+                temp.add(nums[j]);
+            }
+            res.add(checkIsArithmeticSubarrays(temp));
+        }
+        return res;
+    }
+    
+    public boolean checkIsArithmeticSubarrays(List<Integer> nums) {
+        if (nums.size() < 2) return false;
+        Collections.sort(nums);
+        int delta = nums.get(1) - nums.get(0);
+        for (int i=0; i<nums.size() - 1; i++) {
+            if (nums.get(i + 1) - nums.get(i) != delta)
+                return false;
+        }
+        return true;
+    }
+}
--- Day59/Problem_Statement.md
+++ Day59/Problem_Statement.md
@@ -1,4 +1,7 @@
 ## Day 59
 
-Leetcode:Arithmetic Subarrays
+Leetcode: Arithmetic Subarrays
 [SOLVED](https://leetcode.com/problems/arithmetic-subarrays/description/?envType=daily-question&envId=2023-11-23)
+
+GeeksForgeeks: AVL Tree Insertion 
+[Solve Now](https://www.geeksforgeeks.org/problems/avl-tree-insertion/1)
