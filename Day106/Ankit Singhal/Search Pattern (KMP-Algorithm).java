aacaaa149354734bd5757b80e1d1102ee3ed3527
Ankit Singhal <thejiteshsinghal@gmail.com>
2024-01-09 07:37:47 +0530

    ok

---
+++ Day106/Ankit Singhal/Search Pattern (KMP-Algorithm).java
@@ -0,0 +1,26 @@
+class Solution
+{
+    
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        // your code here
+        ArrayList<Integer> list = new ArrayList<>();
+       
+        if(txt.indexOf(pat)==-1) {
+          list.add(-1);
+          return list;
+        }
+        
+        int index=txt.indexOf(pat);
+        list.add(index+1);
+        
+        while(index!=-1) {
+            
+            index=txt.indexOf(pat, index+1);
+            
+            if(index!=-1)
+            list.add(index+1);
+        }
+        return list;
+    }
+}
---
+++ Day106/problem_statement.md
@@ -0,0 +1,7 @@
+## Day 106
+
+LeetCode: Leaf-Similar Trees
+[Solve Now](https://leetcode.com/problems/leaf-similar-trees/description/)
+
+GeeksForGeeks: Search Pattern (KMP-Algorithm) 
+[Solve Now](https://www.geeksforgeeks.org/problems/search-pattern0205/1)
