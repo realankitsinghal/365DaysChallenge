e0e2145f0e26d0f3ed994ec16a36970eb520c8f7
Ankit Singhal <thejiteshsinghal@gmail.com>
2024-01-24 18:42:56 +0530

    24 jan. 2024

---
+++ Day121/Ankit Singhal/eck if a given graph is tree or not.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isTree(int n, int m, ArrayList<ArrayList<Integer>> edges) 
+    {
+        if(m != n-1) return false;
+        // code here
+        int[] parent = new int[n];
+        Arrays.fill(parent,-1);
+        
+        for(ArrayList<Integer> adj : edges){
+            int u = findParent(adj.get(0),parent);
+            int v = findParent(adj.get(1),parent);
+            
+            if(u == v) return false;
+            
+            parent[u] = v;
+        }
+        
+    
+        return true;
+    }
+    private static int findParent(int i,int[] parent){
+        while(parent[i] != -1){
+            i = parent[i];
+        }
+        return i;
+    }
+}
---
+++ Day121/Program_Statement.md
@@ -0,0 +1,7 @@
+## Day 121
+
+LeetCode: Pseudo-Palindromic Paths in a Binary Tree 
+[Solve Now](https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/)
+
+GeeksForGeeks: Check if a given graph is tree or not 
+[Solve Now](https://www.geeksforgeeks.org/problems/is-it-a-tree/1)
