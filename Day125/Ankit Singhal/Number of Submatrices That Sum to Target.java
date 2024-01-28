b5ef4088c279f32410bd99db1a8412703a6dbb76
Ankit Singhal <thejiteshsinghal@gmail.com>
2024-01-28 07:38:31 +0530

    28 jan. 2024

---
+++ Day125/Ankit Singhal/Number of Submatrices That Sum to Target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSubmatrixSumTarget(int[][] mx, int t) {
+        int res = 0, m = mx.length, n = mx[0].length;
+        for (int i = 0;i < m; i++)
+            for (int j = 1; j < n; j++)
+                mx[i][j] += mx[i][j - 1];
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+                Map<Integer, Integer> map = new HashMap<>(); 
+                map.put(0, 1);
+                for (int i = 0, sum = 0; i < m; i++) {
+                    sum += (mx[i][r] - (l == 0 ? 0 : mx[i][l - 1]));
+                    res += map.getOrDefault(sum - t, 0);
+                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ Day125/Program_Statement.md
@@ -0,0 +1,7 @@
+## Day 125
+
+LeetCode: Number of Submatrices That Sum to Target 
+[Solve Now](https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/description/)
+
+GeeksForGeeks: Geekina Hate 1s 
+[Solve Now](https://www.geeksforgeeks.org/problems/geekina-hate-1s/1)
