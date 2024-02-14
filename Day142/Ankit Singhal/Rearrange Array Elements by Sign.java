1a2deddca5e582395c1706e9a3df1bbf02dca33b
Ankit Singhal <thejiteshsinghal@gmail.com>
2024-02-14 20:12:14 +0530

    14 feb. 2024

--- Day142/Ankit Singhal/Rearrange Array Elements by Sign.java
+++ Day142/Ankit Singhal/Rearrange Array Elements by Sign.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        int p=0, n=1;
+        int p=0, ng=1;
         
         for(int i=0; i<n; i++){
             if(nums[i]>0){
@@ -10,8 +10,8 @@ class Solution {
                 p+=2;
             }
             else{
-                res[n]=nums[i];
-                n+=2;
+                res[ng]=nums[i];
+                ng+=2;
             }
         }    
         return res;
