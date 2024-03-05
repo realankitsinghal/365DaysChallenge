33108184e8d1f1ddea76dc93da4b28ebc6972c66
Ankit Singhal <thejiteshsinghal@gmail.com>
2024-03-05 21:11:55 +0530

    5 march 2023

---
+++ Day162/Ankit Singhal/Minimum Length of String After Deleting Similar Ends.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumLength(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                char ch = s.charAt(i);
+                while (i < n && s.charAt(i) == ch)
+                    i++;
+
+                while (j > i && s.charAt(j) == ch)
+                    j--;
+            } else
+                break;
+        }
+        return j - i + 1;
+    }
+}
---
+++ Day162/Problem_Statement.md
@@ -0,0 +1,7 @@
+## Day 162
+
+LeetCode: Minimum Length of String After Deleting Similar Ends 
+[Solve Now](https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/description/)
+
+GeeksForGeeks: Maximum Index 
+[Solve Now](https://www.geeksforgeeks.org/problems/maximum-index-1587115620/1)
