c2275a3d9dd7d97182d6badef8011cc54b2d6c66
Sankalp <sanklpkaushik21@gmail.com>
2024-05-04 06:34:50 +0530

    4 May 2024

---
+++ Day222/Sankalp Haritash/Boats to Save People.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int i=0,j=people.length-1;
+        int b=0;
+
+        while(i<=j){
+
+            if(people[i]+people[j]<=limit)
+                i++;
+
+            j--;
+
+            b++;
+
+        }
+
+        return b;
+    }
+}
