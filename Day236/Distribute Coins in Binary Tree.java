ae82ce349232df7842ba93d6a1ee59a2e2c22114
Sankalp <sanklpkaushik21@gmail.com>
2024-05-18 05:56:06 +0530

    18 May 2024

---
+++ Day236/Distribute Coins in Binary Tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int moves;
+    public int distributeCoins(TreeNode root) {
+
+        moves = 0;
+        dfs(root);
+        return moves;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftBalance = dfs(node.left);
+        int rightBalance = dfs(node.right);
+
+        int balance = node.val + leftBalance + rightBalance - 1;
+        moves += Math.abs(balance);
+
+        return balance;
+    }
+}
